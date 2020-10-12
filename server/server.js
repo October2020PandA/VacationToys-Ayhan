5928b13d90c1222fff38509f01d39a57fd0f4cee
Ayhan Durak <ayhandurak@Ayhans-MacBook-Pro.local>
2020-10-12 16:55:41 -0700

    add mailer

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -11,5 +12,30 @@ require("./models/reservation.model");
 require("./config/mongoose.config")
 
 require("./routes/reservation.routes")(app);
+app.post('/send',(req,sendRes) => {
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: '**',
+            pass: '**'
+        }
+    });
+    var mailOptions = {
+        from: 'ayhandurak87@gmail.com',
+        to: req.body.to,
+        subject: 'About your reservation',
+        text: req.body.text
+    };
+    console.log('created');
+    transporter.sendMail(mailOptions,  function(err, res) {
+        if (err) {
+            sendRes.send({"message": `It was an error: ${err}`, "code": "fail"})
+          console.error('there was an error: ', err);
+        } else {
+            sendRes.send({"message": "Mail went successfully", "code": "success"})
+          console.log('here is the res: ', res)
+        }
+    });
+})
 
 app.listen(8000, () => console.log("The server is listening on port 8000"))
